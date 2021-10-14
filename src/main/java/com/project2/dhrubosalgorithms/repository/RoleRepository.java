03118b30f5e819f9f9204ee36de943fa91146565
Matthew James <matjames@paypal.com>
2021-10-14 09:08:57 -0500

    updated Role Repository

--- src/main/java/com/project2/dhrubosalgorithms/repository/RoleRepository.java
+++ src/main/java/com/project2/dhrubosalgorithms/repository/RoleRepository.java
@@ -1,9 +1,11 @@
 package com.project2.dhrubosalgorithms.repository;
 
 import com.project2.dhrubosalgorithms.model.Role;
+import com.project2.dhrubosalgorithms.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
+    Role findByName(String name);
 
 }
